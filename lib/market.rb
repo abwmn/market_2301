cb8992626d7e682cf3d9d56c395e140343a34e1b
Andrew Bowman <andrewtbowman@gmail.com>
2023-03-07 11:51:19 -0500

    feat: market

--- lib/market.rb
+++ lib/market.rb
@@ -1,3 +1,28 @@
 class Market
+  attr_reader :name, :vendors
+
+  def initialize(name)
+    @name = name
+    @vendors = []
+  end
   
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    names = []
+    @vendors.each do |vendor|
+      names << vendor.name
+    end
+    names
+  end
+
+  def vendors_that_sell(item)
+    vendors = []
+    @vendors.each do |vendor|
+      vendors << vendor if vendor.check_stock(item) > 0
+    end
+    vendors
+  end
 end
