e1c6a255c7048a483673bc4feb6abe788b9ceeb2
Andrew Bowman <andrewtbowman@gmail.com>
2023-03-07 12:10:17 -0500

    fix vendor spec for revenue

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -1,9 +1,10 @@
 require './lib/vendor.rb'
+require './lib/item.rb'
 
 RSpec.describe Vendor do
   before(:each) do
     @vendor = Vendor.new("Rocky Mountain Fresh")
-    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item1 = Item.new({name: 'Peach', price: "$0.79"})
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
 
@@ -29,8 +30,9 @@ RSpec.describe Vendor do
   it 'calculates potential revenue' do
     @vendor.stock(@item1, 28)
     @vendor.stock(@item2, 20)
-    expect(@vendor.potential_revenue).to eq(31)
-    expect(@item1.price).to eq('$0.75')
+    expect(@vendor.potential_revenue).to eq(32.12)
+    expect(@item1.price).to eq('$0.79')
+    #changed price to make sure it can handle odd prices
   end
 end
 
