55703609fae038a6544bb94d5f6d67bde6990b72
Andrew Bowman <andrewtbowman@gmail.com>
2023-03-07 11:27:28 -0500

    feat: vendor

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,3 +1,24 @@
 class Vendor
-  
+  attr_reader :name, :inventory
+  def initialize(name)
+    @name = name
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    @inventory[item]
+  end
+
+  def stock(item, qty)
+    @inventory[item] += qty
+  end
+
+  def potential_revenue
+    total = 0
+    @inventory.each do |item, qty|
+      price = item.price.delete('$').to_f
+      total += price * qty
+    end
+    total
+  end
 end
