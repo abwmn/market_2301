7efa2fe21a4ae0896b7427bed6b4bc9503a8f809
Andrew Bowman <andrewtbowman@gmail.com>
2023-03-07 11:27:40 -0500

    test: item

---
+++ spec/item_spec.rb
@@ -0,0 +1,17 @@
+require './lib/item.rb'
+
+RSpec.describe Item do
+  before(:each) do
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  it 'exists' do
+    expect(@item1).to be_a(Item)
+  end
+
+  it 'has attributes' do
+    expect(@item1.name).to eq('Peach')
+    expect(@item1.price).to eq('$0.75')
+  end
+end
